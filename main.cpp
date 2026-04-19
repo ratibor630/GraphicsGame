0cf6d96df3fe9e160bfc8daf13fd442be93a5f02
ter4 <ter4diagnozb@mail.ru>
2026-04-20 02:31:54 +0500

    v.0.0.1b working - добавлена текстура ходьбы главного персонажа - реализована безусловная анимация ходьбы - добавлены ресурсы шрифтов - добавлен курсор

--- main.cpp
+++ main.cpp
@@ -1,22 +1,30 @@
-//Using SDL, standard IO, and strings
 #include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
-	window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+	const int RESOLUTION_WIDTH = 800;
+	const int RESOLUTION_HEIGHT = 800;
+	window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, RESOLUTION_WIDTH, RESOLUTION_HEIGHT, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	
 	bool running = true;
+	const int PLAYER_WALKING_SPRITE_COUNT = 29;
+	const int ANIMATION_SPEED = 32;
+	int playerSpriteCountCurrent = 0;
 	SDL_Event event;
 
-	SDL_Rect rect;
-	rect.w = 100;
-	rect.h = 100;
-	rect.x = 0;
-	rect.y = 0;
+	SDL_Texture* playerWalkingTexture = IMG_LoadTexture(renderer, "resources/images/player/walking_middle.png");
+	SDL_Rect playerWalkingSrc = { 0, 0, 400, 400 };
+	SDL_Rect playerWalkingDest = { 0, 0, 400, 400 };
+
+	Uint32 lastUpdate = SDL_GetTicks();
 
 	while (running)
 	{
@@ -69,23 +77,19 @@ int main(int argc, char* argv[]) {
 				{
 				case SDL_SCANCODE_W: {
 					cout << "W pressed" << endl;
-					rect.y -= 1;
 					break;
 
 				}
 				case SDL_SCANCODE_A: {
 					cout << "A pressed" << endl;
-					rect.x -= 1;
 					break;
 				}
 				case SDL_SCANCODE_S: {
 					cout << "S pressed" << endl;
-					rect.y += 1;
 					break;
 				}
 				case SDL_SCANCODE_D: {
 					cout << "D pressed" << endl;
-					rect.x += 1;
 					break;
 				}
 				default:
@@ -123,10 +127,17 @@ int main(int argc, char* argv[]) {
 		}
 		
 		//Отрисовываем фон
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_SetRenderDrawColor(renderer, 225, 255, 255, 255);
 		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
-		SDL_RenderFillRect(renderer, &rect);
+
+		Uint32 currentTicks = SDL_GetTicks();
+		if (currentTicks - lastUpdate >= ANIMATION_SPEED) {
+			playerSpriteCountCurrent = (playerSpriteCountCurrent + 1) % PLAYER_WALKING_SPRITE_COUNT;
+			playerWalkingSrc.x = playerWalkingSrc.w * playerSpriteCountCurrent;
+			lastUpdate = currentTicks;
+		}
+
+		SDL_RenderCopy(renderer, playerWalkingTexture, &playerWalkingSrc, &playerWalkingDest);
 
 		SDL_RenderPresent(renderer);
 
